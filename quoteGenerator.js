b806bd2d4cbbc2ba23a95298081ea27a7908dc78
willybeans <willwedmedyk@gmail.com>
2019-03-18 09:55:42 -0400

    removed global var's. updated to let

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,36 +1,35 @@
-var quoteData = [];
-var quoteText;
-var quoteAuthor;
-
-function newQuote () {
-      $.ajax({
-  dataType: "json",
-  url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-  success: function( data ) {
-    quoteData = data.shift();
-    quoteText = quoteData.content;
-    quoteAuthor = quoteData.title;
-    $(".quoteMark").addClass("fa-quote-left");
-    $(".quoteAuthor").text(" -- " + quoteAuthor);
-    $(".quoteContent").html(quoteText);
-    console.log(quoteData);
-   },
-   error: function(xhr, status, error) {
-      console.log("error");
-   },
-      cache: false //this is important
-});
-}
-function tweet() {
-  var content = $(".quoteContent").text();
-  var author = $(".quoteAuthor").text();
-  var text = content + author;
-  var url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text);
-  window.open(url);
-  return false;
-}
-
 $(document).ready(function() {
   newQuote();
   $(".newQuote").on('click', newQuote);
 }); 
+
+function newQuote () {
+  let quoteData = [];
+  let quoteText;
+  let quoteAuthor;
+  $.ajax({
+    dataType: "json",
+    url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    success: function( data ) {
+      quoteData = data.shift();
+      quoteText = quoteData.content;
+      quoteAuthor = quoteData.title;
+      $(".quoteMark").addClass("fa-quote-left");
+      $(".quoteAuthor").text(" -- " + quoteAuthor);
+      $(".quoteContent").html(quoteText);
+      console.log(quoteData);
+     },
+     error: function(xhr, status, error) {
+        console.log("error");
+     },
+     cache: false //this is important
+  });
+}
+function tweet() {
+  let content = $(".quoteContent").text();
+  let author = $(".quoteAuthor").text();
+  let text = content + author;
+  let url = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(text);
+  window.open(url);
+  return false;
+}
